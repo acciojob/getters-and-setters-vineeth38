ad45147eb28e007ed7ee8e65a4b95e88f0e29330
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-15 17:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,4 +24,14 @@ class Teacher extends Person {
     console.log(`${this._name} is teaching.`);
   }
 }
+const john = new Person("John Doe", 30);
+console.log(john.name); // Output: John Doe
+john.age = 35; // Set the age using the setter
+console.log(john.age); // Output: 35
+
+const alice = new Student("Alice Smith", 20);
+alice.study(); // Output: Alice Smith is studying.
+
+const mrSmith = new Teacher("Mr. Smith", 45);
+mrSmith.teach(); // Output: Mr. Smith is teaching.
 
