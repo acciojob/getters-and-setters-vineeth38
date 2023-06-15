508e0449beeaab1023fe033f531d815639c946cb
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-15 17:18:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(value) {
+    this._age = value;
+  }
+}
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+class Student extends Person {
+  study() {
+    console.log(`${this._name} is studying.`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this._name} is teaching.`);
+  }
+}
+
