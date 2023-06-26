330608f4d45765fe8d1a0e9b9fc65fdc1e09809d
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-26 17:40:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
-
-//complete this code
 class Person {
-    age;
-    name;
-    constructor(name,age){
-      this.name=name;
-      this.age=age;
-    }
-    set setAge(age){
-        this.age=age;
-    }
-    get getName(){
-        return this.name;
-    }
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+ 
+  get name() {
+    return this._name;
+  }
+ 
+  /**
+   * @param {any} age
+   */
+  set age(age) {
+    this._age = age;
+  }
 }
-
+ 
 class Student extends Person {
-    constructor(name,age){
-        super(name,age);
-    }
-    study(){
-        console.log(`${this.name} is studying`);
-    }
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
-
+ 
 class Teacher extends Person {
-    constructor(name,age){
-        super(name,age);
-    }
-    teach(){
-        console.log(`${this.name} is teaching`);
-    }
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-
 let student1=new Student('Ramesh',21);
 console.log(student1);
 // student1.teach();
@@ -43,4 +37,5 @@ teacher1.teach();
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
+window.Teacher = Teacher;
 
