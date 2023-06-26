23a41963030921059b2752ffeae1a7d66d443bb5
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-26 16:31:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
+
+//complete this code
 class Person {
-  constructor(name, age) {
-      this.name = name;
-    this.year = year;
-  }
-
-  get pnam() {
-    return this.name;
-  }
-
-  set age(x) {
-    this.age= x;
-  }
+    age;
+    name;
+    constructor(name,age){
+      this.name=name;
+      this.age=age;
+    }
+    set setAge(age){
+        this.age=age;
+    }
+    get getName(){
+        return this.name;
+    }
 }
 
 class Student extends Person {
-  study() {
-    console.log(`${this.name} is studying.`);
-  }
+    constructor(name,age){
+        super(name,age);
+    }
+    study(){
+        console.log(`${this.name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-  teach() {
-    console.log(`${this.name} is teaching.`);
-  }
+    constructor(name,age){
+        super(name,age);
+    }
+    teach(){
+        console.log(`${this.name} is teaching`);
+    }
 }
-const john = new Person("John Doe", 30);
-console.log(john.name); // Output: John Doe
-john.age = 35; // Set the age using the setter
-console.log(john.age); // Output: 35
 
-const alice = new Student("Alice Smith", 20);
-alice.study(); // Output: Alice Smith is studying.
+let student1=new Student('Ramesh',21);
+console.log(student1);
+// student1.teach();
+student1.study();
+let teacher1=new Teacher('Max',30);
+teacher1.teach();
 
-const mrSmith = new Teacher("Mr. Smith", 45);
-mrSmith.teach(); // Output: Mr. Smith is teaching.
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
 
